8c6ea2f6c1c32621684af33352acd77083ebed53
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-11-05 04:15:29 -0600

    added React

---
+++ frontend1/rainbowreturns/src/App.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// //import Navbar from './components/Navbar';
+// //import './App.css';
+// import Opening from './components/pages/Opening';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// //import Services from './components/pages/Services';
+// //import Products from './components/pages/Products';
+// //import SignUp from './components/pages/SignUp';
+
+// function App() {
+//   return (
+//     <>
+//       <Router>
+//         <Navbar />
+//         <Switch>
+//           <Route path='/' exact component={Opening} />
+//           {/* <Route path='/services' component={Services} />
+//           <Route path='/products' component={Products} />
+//           <Route path='/sign-up' component={SignUp} /> */}
+//         </Switch>
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
---
+++ frontend1/rainbowreturns/src/pages/opening.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// // import '../../App.css';
+// // import Cards from '../Cards';
+// // import HeroSection from '../HeroSection';
+// // import Footer from '../Footer';
+
+// function Opening() {
+//   return (
+//     <>
+//       {/* <HeroSection />
+//       <Cards />
+//       <Footer /> */}
+//       <h1>Hello World!</h1>
+//     </>
+//   );
+// }
+
+// export default Opening;
