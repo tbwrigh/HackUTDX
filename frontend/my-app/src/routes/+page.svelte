a7730d662da0984ca29084a272400505bcae8693
megankulshekar <91085343+megankulshekar@users.noreply.github.com>
2023-11-05 00:32:21 -0500

    added page for Auth0

--- frontend/my-app/src/routes/+page.svelte
+++ frontend/my-app/src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import { withAuth } from "./auth"
+    const auth=withAuth()
+</script>
 <div class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-red-500 via-yellow-500 via-green-500 via-blue-500 via-indigo-500 to-purple-500">
     <h1 class="text-5xl font-bold text-white">
         Welcome to Rainbow Returns!
@@ -5,8 +9,7 @@
     <h2 class ="text-2x1 font-bold text-white m-10">
         The Stock Price Portfolio Catered To Your Preferences
     </h2>
-    <a href="/home"><button class="bg-violet-500 py-2 px-4 rounded m-3">
+    <button class="bg-violet-500 py-2 px-4 rounded m-3" on:click={auth.login}>
         Sign Up/Log In
     </button>
-    </a>
 </div>
--- frontend/my-app/src/routes/app.svelte
+++ frontend/my-app/src/routes/app.svelte
@@ -1,12 +1,96 @@
-<script>
+<script lang="ts">
     import { Router, Route } from 'svelte-routing';
-  
     import Home from './+page.svelte';
     import Opening from './home/+page.svelte';
-    // import SecondPage from './SecondPage.svelte';
-  </script>
-  
-  <Router>
+
+    import type { User } from '@auth0/auth0-spa-js';
+    import { withAuth } from './auth';
+
+    const auth = withAuth();
+
+    let user: User;
+    auth.user.subscribe(userAuth => {
+        user = userAuth;
+    });
+
+    let token = "";
+    auth.token.subscribe(t => {
+        token = t;
+    });
+</script>
+
+<Router>
     <Route path="/" component={Opening} />
     <Route path="/home" component={Home} />
-  </Router>
+</Router>
+
+<main>
+    <div>
+        {#if !user}
+            <button on:click={auth.login}>Login</button>
+        {:else}
+            <button on:click={auth.logout}>Logout</button>
+            <h1>Hello {user.nickname}</h1>
+            <div class="profile">
+                <img class="profile" src={user.picture} alt="User profile">
+            </div>
+            <p>User:</p>
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+            <p>Access Token:</p>
+            <!-- Don't display access tokens -->
+            <pre>DON'T DISPLAY ACCESS TOKENS</pre>
+        {/if}
+    </div>
+</main>
+
+<style>
+    * {
+        box-sizing: border-box;
+    }
+
+    button {
+        padding: 1rem;
+        background-color: aqua;
+        color: black;
+        transition: background-color 500ms ease;
+    }
+
+    button:hover {
+        background-color: aquamarine;
+    }
+
+    pre {
+        max-width: 100%;
+        word-wrap: break-word;
+        text-align: left;
+    }
+
+    img.profile {
+        border-radius: 50%;
+        border: 0.5rem solid aqua;
+        opacity: 0.8;
+        transition: opacity 500ms ease, border-color 500ms ease;
+    }
+
+    img.profile:hover {
+        opacity: 1;
+        border-color: aquamarine;
+    }
+
+    div.profile {
+        margin: 1rem;
+    }
+
+    pre {
+        opacity: 0.5;
+        border: 1px solid black;
+        transition: opacity 500ms ease, border-color 500ms ease;
+        padding: 1rem;
+        white-space: pre-wrap;
+    }
+
+    pre:hover {
+        opacity: 1;
+        border-color: aquamarine;
+    }
+</style>
