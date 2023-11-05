16b2a5ddd21dec8c8912416f153ed1f59447f837
Tyler Wright <tyler.wright5146@gmail.com>
2023-11-05 06:20:04 -0600

    add random for stock value in event api fails (which it mostly does)

--- backend/src/main/kotlin/co/rainbowreturns/models/Stock.kt
+++ backend/src/main/kotlin/co/rainbowreturns/models/Stock.kt
@@ -12,11 +12,15 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
+import java.util.Random
 
 @Serializable
 data class AlphaVantageResponse(val `Time Series (Daily)`: JsonObject)
 
-suspend fun fetchCurrentPrice(symbol: String): JsonObject? {
+@Serializable
+data class AlphaVantageDayResponse(val open: Int, val high: Int, val low: Int, val close: Int, val volume: Int)
+
+suspend fun fetchCurrentPrice(symbol: String): Double {
     val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json(Json {
@@ -25,16 +29,27 @@ suspend fun fetchCurrentPrice(symbol: String): JsonObject? {
         }
     }
 
-    val apiKey = PropertiesLoader.Companion.properties?.get("STOCK_API_KEY")
+    val apiKey = PropertiesLoader.Companion.properties?.get("STOCK_API_KEY").toString()
+
     val url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=$symbol&apikey=$apiKey"
     try {
         val response: AlphaVantageResponse = client.get(url).body()
 
         client.close()
 
-        return response.`Time Series (Daily)`
+        val most_recent = response.`Time Series (Daily)`.keys.sortedBy { date ->
+            val parts = date.split("-")
+            parts[0].toInt()*10000 + parts[1].toInt() * 100 + parts[2].toInt()
+        }.max()
+
+        val day = response.`Time Series (Daily)`["1. open"]
+
+
+        return day.toString().toDouble()
     }catch (e: Exception) {
-        return null
+        // due to cost restrictions, we aren't paying for the api and there is very limited calls to it
+        val rand = Random()
+        return rand.nextDouble() * (175 - 30) + 30
     }
 }
 
